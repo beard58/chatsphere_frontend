60008d22e1ec9400af27ddfc7b3182918e748653
beard58 <borkarkiran58@gmail.com>
2025-05-30 15:28:43 +0530

    tailwind css

--- src/ui/components/Home.js
+++ src/ui/components/Home.js
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
       <p>This is the home page of our application.</p>
     </div>
   );
